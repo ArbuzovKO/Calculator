2741e2c00cf3535f5f64deef633b511bfda29d52
ArbuzovKO <121497979+ArbuzovKO@users.noreply.github.com>
2023-01-21 02:09:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let runnindTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value)
+  }
+  screen.innerText = buffer
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case 'C':
+      buffer = '0'
+      runnindTotal = 0
+      break;
+    case '=':
+      if (previousOperator === null) {
+        return
+      }
+      flushOperation(parceInt(buffer));
+      previousOperator = null;
+      buffer = runnindTotal;
+      runnindTotal = 0;
+      break;
+    case '←':
+      if (buffer.length === 1) {
+        buffer = '0';
+      } else {
+        buffer = buffer.toString(0, buffer.length - 1);
+      }
+      break;
+  }
+}
